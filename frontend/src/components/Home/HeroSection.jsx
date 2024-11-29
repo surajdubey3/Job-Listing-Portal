9817584676fbf632fbc189b5c33219c052933a5a
surajdubey3 <surajdubey0317@gmail.com>
2024-11-30 00:26:59 +0530

    Reinitialized repository

---
+++ frontend/src/components/Home/HeroSection.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { FaBuilding, FaSuitcase, FaUsers, FaUserPlus } from "react-icons/fa";
+
+const HeroSection = () => {
+  const details = [
+    {
+      id: 1,
+      title: "1,23,441",
+      subTitle: "Live Job",
+      icon: <FaSuitcase />,
+    },
+    {
+      id: 2,
+      title: "91220",
+      subTitle: "Companies",
+      icon: <FaBuilding />,
+    },
+    {
+      id: 3,
+      title: "2,34,200",
+      subTitle: "Job Seekers",
+      icon: <FaUsers />,
+    },
+    {
+      id: 4,
+      title: "1,03,761",
+      subTitle: "Employers",
+      icon: <FaUserPlus />,
+    },
+  ];
+  return (
+    <>
+      <div className="heroSection">
+        <div className="container">
+          <div className="title">
+            <h1>Your Dream Job Awaits</h1>
+            <h1>Find it Here</h1>
+            <p>
+            Your ultimate destination for finding the perfect job.
+            Whether you're starting your career, looking to advance,
+            or seeking a change, we connect you with top employers across
+            diverse industries. Explore thousands of job listings, get 
+            personalized matches, and access valuable career resources.
+            Our user-friendly platform makes job searching easy and efficient. 
+            Create your profile, search and apply for jobs, and get noticed by
+            leading companies. Join our thriving community and take the next step
+             in your career journey. Start your job search today with Job Portal 
+             – where your career success begins.
+            </p>
+          </div>
+          <div className="image">
+            <img src="/heroS.jpg" alt="hero" />
+          </div>
+        </div>
+        <div className="details">
+          {details.map((element) => {
+            return (
+              <div className="card" key={element.id}>
+                <div className="icon">{element.icon}</div>
+                <div className="content">
+                  <p>{element.title}</p>
+                  <p>{element.subTitle}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeroSection;
